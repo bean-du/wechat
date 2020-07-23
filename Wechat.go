0d997ec0364c2ed1ec3298e74683516ca56710f9
Viven <binit@sina.cn>
2020-07-23 18:02:07 +0800

    first push

--- Wechat.go
+++ Wechat.go
@@ -1,7 +1,7 @@
 package wechat
 
 // api url don't need "https" and route, like this "oapi.campus.qq.com"
-func NewWeChat(secretId int, secretKey, apiUrl string) *Wechat {
+func NewWeChat(secretId int, secretKey, apiUrl string) *WeChat {
 	return &WeChat{
 		SecretId:  secretId,
 		SecretKey: secretKey,
